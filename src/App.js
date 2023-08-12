b62cb95c81f5a20a337af17ccc018b30bf87553b
Manuel Lanada <manuellanada@outlook.com>
2023-08-12 14:30:29 +0800

    again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          manuellanada awesomeness test!
+          manuellanada awesomeness!
         </p>
         <a
           className="App-link"
